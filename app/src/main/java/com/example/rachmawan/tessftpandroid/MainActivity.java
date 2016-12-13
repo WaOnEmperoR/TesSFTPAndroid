6d2351ed7d1c2138a7e3af0966714576358e2699
WaOnEmperoR <themusketeer17@gmail.com>
2016-12-13 20:53:48 +0700

    update-02

--- app/src/main/java/com/example/rachmawan/tessftpandroid/MainActivity.java
+++ app/src/main/java/com/example/rachmawan/tessftpandroid/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,6 +18,13 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.xml.sax.InputSource;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
     private String imgPath;
     private EditText edtFilePath;
     private EditText edtPassphrase;
+    private EditText edtJSON;
     private Button btnHitungLuas;
     private Button btnPindah;
     private Button btnQR;
@@ -89,6 +98,12 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void getDataJSON()
+    {
+        new RetrieveFeedTask().execute("");
+
+    }
+
     private void initUI() {
         btnFileChooser = (Button) findViewById(R.id.btn_choose);
         btnUpload = (Button) findViewById(R.id.btn_upload);
@@ -96,6 +111,7 @@ public class MainActivity extends AppCompatActivity {
         btnSigning = (Button) findViewById(R.id.btn_Signing);
         edtFilePath = (EditText) findViewById(R.id.edt_filename);
         edtPassphrase = (EditText) findViewById(R.id.edt_passphrase);
+        edtJSON = (EditText) findViewById(R.id.edtBalikan);
         progBar_UD = (ProgressBar) findViewById(R.id.progBar_updown);
         radioGroup_user = (RadioGroup) findViewById(R.id.radio_user);
         radio_asep = (RadioButton) findViewById(R.id.radioAsep);
@@ -105,7 +121,6 @@ public class MainActivity extends AppCompatActivity {
 
     private void initEvent(){
         btnFileChooser.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
                 showP12Chooser();
@@ -136,4 +151,59 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
     }
 
+    /*
+    private void getJSON(String myurl) throws IOException {
+        URL url = new URL(myurl);
+
+        Log.d("dhedy", "The response is: " + myurl);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setReadTimeout(10000  milliseconds );
+        //conn.setConnectTimeout(15000  milliseconds );
+        /*conn.setRequestMethod("POST");
+        conn.setDoInput(true);
+
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            params.add(new BasicNameValuePair("token", token));
+            params.add(new BasicNameValuePair("token_app", token_app));
+            params.add(new BasicNameValuePair("username", serialnumber));
+
+        String urlParameters = "token="+token+"&token_app="+token_app+"&username="+serialnumber;
+        conn.getOutputStream().write(urlParameters.getBytes("UTF-8"));
+
+        conn.connect();
+
+        int response = conn.getResponseCode();
+        Log.d("dhedy", "The response is: " + response);
+        is = conn.getInputStream();
+
+        // Convert the InputStream into a string
+        String contentAsString = convertInputStreamToString(is, length);
+        return contentAsString;
+
+    }*/
+
+    public class RetrieveFeedTask extends AsyncTask<String, Void, String> {
+
+        private Exception exception;
+
+        protected String doInBackground(String... urls) {
+            try {
+                URL url = new URL(urls[0]);
+                InputSource is = new InputSource(url.openStream());
+                String jadi = is.toString();
+
+                return jadi;
+            } catch (Exception e) {
+                this.exception = e;
+
+                return "";
+            }
+        }
+
+        protected void onPostExecute(String hasil) {
+            edtJSON.setText(hasil);
+            // TODO: check this.exception
+            // TODO: do something with the feed
+        }
+    }
 }
